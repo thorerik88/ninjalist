a78fdcbd746f3982ff37c1d990d57aa8a9008ee3
Thor-Erik Støvland <thor-erik.stovland@oo.no>
2022-02-24 20:27:02 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Footer from "../comps/Footer";
+import Navbar from "../comps/Navbar";
+
+const About  = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Reprehenderit commodo deserunt exercitation sunt id. Consequat ut fugiat commodo incididunt sint velit minim anim veniam occaecat irure proident consequat Lorem. Eiusmod et cillum magna sit. Dolor labore qui amet amet eiusmod in magna nisi. Ad est est occaecat ea in dolore cillum laborum ullamco nulla enim. Labore nulla amet cillum commodo velit excepteur elit.</p>
+            <p>Reprehenderit commodo deserunt exercitation sunt id. Consequat ut fugiat commodo incididunt sint velit minim anim veniam occaecat irure proident consequat Lorem. Eiusmod et cillum magna sit. Dolor labore qui amet amet eiusmod in magna nisi. Ad est est occaecat ea in dolore cillum laborum ullamco nulla enim. Labore nulla amet cillum commodo velit excepteur elit.</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,12 @@
+import Footer from "../../comps/Footer";
+import Navbar from "../../comps/Navbar";
+
+const Ninjas = () => {
+    return ( 
+        <div>
+            <h1>All Ninjas</h1>
+        </div>
+     );
+}
+ 
+export default Ninjas;
